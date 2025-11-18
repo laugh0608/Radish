9b6e934b19ed678acc04c7bda3e90b49ecbb3e8a
laugh0608 <laug608@163.com>
2025-11-18 22:47:25 +0800

    fix: 完善了一下automapper的配置

--- Radish.Extension/AutoMapperExtension/AutoMapperConfig.cs
+++ Radish.Extension/AutoMapperExtension/AutoMapperConfig.cs
@@ -4,12 +4,40 @@ using Radish.Extension.AutoMapperExtension.CustomProfiles;
 namespace Radish.Extension.AutoMapperExtension;
 
 /// <summary>集中注册 AutoMapper 配置。</summary>
-public static class AutoMapperConfig
+public class AutoMapperConfig
 {
-    public static void RegisterProfiles(IMapperConfigurationExpression cfg)
+    public static void RegisterCustomProfile(IMapperConfigurationExpression cfg)
     {
         cfg.AddProfile<CustomProfile>();
+    }
+    public static void RegisterProfiles(IMapperConfigurationExpression cfg)
+    {
         cfg.AddProfile<RoleProfile>();
         cfg.AddProfile<UserProfile>();
     }
+    
+    // private ILoggerFactory _loggerFactory;
+    // private ILogger _logger;
+
+    // public AutoMapperConfig(ILoggerFactory loggerFactory, ILogger logger)
+    // {
+    //     _loggerFactory = loggerFactory;
+    //     _logger = logger;
+    // }
+
+    // public MapperConfiguration RegisterMappings()
+    // {
+    //     var licenseKey = AppSettingsTool.RadishApp(new string[] {"AutoMapper", "LicenseKey"}).ObjToString();
+    //     if (string.IsNullOrWhiteSpace(licenseKey))
+    //     {
+    //         _logger.LogDebug("AutoMapper LicenseKey is null or empty");
+    //     }
+    //     return new MapperConfiguration(cfg =>
+    //     {
+    //         cfg.LicenseKey = licenseKey;
+    //         cfg.AddProfile<CustomProfile>();
+    //         cfg.AddProfile<RoleProfile>();
+    //         cfg.AddProfile<UserProfile>();
+    //     }, _loggerFactory);
+    // }
 }
