b8219271bbc3032841504d43d3223ea48ab9dc89
laugh0608 <laug608@163.com>
2025-11-20 21:02:50 +0800

    chore: save local changes

--- Radish.IService/IBaseService.cs
+++ Radish.IService/IBaseService.cs
@@ -1,14 +1,49 @@
+using System.Linq.Expressions;
+
 namespace Radish.IService;
 
 public interface IBaseService<TEntity, TVo> where TEntity : class
 {
+    #region 增
+
     /// <summary>写入一条实体类数据</summary>
     /// <param name="entity">泛型实体类</param>
     /// <returns>插入数据的 SnowflakeId, 类型为 long</returns>
     Task<long> AddAsync(TEntity entity);
 
+
+    /// <summary>分表-写入实体数据</summary>
+    /// <param name="entity">泛型实体类</param>
+    /// <returns>插入数据的 SnowflakeId, 类型为 long</returns>
+    Task<List<long>> AddSplitAsync(TEntity entity);
+
+    #endregion
+
+    #region 删
+
+    // 删
+
+    #endregion
+
+    #region 改
+
+    // 改
+
+    #endregion
+
+    #region 查
+
     /// <summary>按照泛型实体类查询表中所有数据</summary>
     /// <remarks>已加入实体类和视图模型的泛型对象关系映射</remarks>
     /// <returns>List TEntity</returns>
     Task<List<TVo>> QueryAsync();
+
+    /// <summary>分表-按照泛型实体类查询表中所有数据</summary>
+    /// <param name="whereExpression">条件表达式</param>
+    /// <param name="orderByFields">排序字段，默认为 Id，其他如 Name, Age</param>
+    /// <returns>List TEntity</returns>
+    Task<List<TEntity>> QuerySplitAsync(Expression<Func<TEntity, bool>> whereExpression,
+        string orderByFields = "Id");
+
+    #endregion
 }
--- Radish.Model/LogModels/AuditSqlLog.cs
+++ Radish.Model/LogModels/AuditSqlLog.cs
@@ -2,11 +2,11 @@ using SqlSugar;
 
 namespace Radish.Model.LogModels;
 
-/// <summary>数据库日志模型类</summary>
+/// <summary>SQL 审计日志模型类</summary>
 /// <remarks>按月分表(自带分表支持: 年、季、月、周、日)</remarks>
 [Tenant(configId: "Log")] // 就是在 appsetting 中配置的 "ConnId": "Log"
-[SplitTable(SplitType.Month)]
-[SugarTable($@"{nameof(AuditSqlLog)}_{{year}}{{month}}{{day}}")] // [SugarTable("AuditSqlLog_20231201")]
+[SplitTable(SplitType.Month)] // 目前是按月分表
+[SugarTable($@"{nameof(AuditSqlLog)}_{{year}}{{month}}{{day}}")] // [SugarTable("AuditSqlLog_20231201")]，标准格式，不能更改
 public class AuditSqlLog : BaseLog
 {
 }
