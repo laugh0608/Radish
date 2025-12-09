4f0eca5f99fca84e4686c7256e6ef1712d430ab7
laugh0608 <laugh0608@foxmail.com>
2025-12-09 22:02:24 +0800

    docs: 更新了一下租户相关的注释

--- Radish.Model/Tenants/SubLibBusinessTable.cs
+++ Radish.Model/Tenants/SubLibBusinessTable.cs
@@ -2,10 +2,14 @@ using Radish.Common.TenantTool;
 using Radish.Model.Root;
 
 namespace Radish.Model.Tenants;
+
 /// <summary>
 /// 分库，测试业务数据
 /// </summary>
-/// <remarks>多租户 (不同 TenantId 的数据在不同的数据库里隔离)</remarks>
+/// <remarks>
+/// <para>多租户 (不同 TenantId 的数据在不同的数据库里隔离)</para>
+/// <para>需要配置 Tenant 表中的数据库连接字符串</para>
+/// </remarks>
 [MultiTenant(TenantTypeEnum.DataBases)]
 public class SubLibBusinessTable : RootEntityTKey<long>
 {
