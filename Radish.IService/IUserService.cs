858e5dccd27209d7f51409b0ee26e6a1e8737500
laugh0608 <laugh0608@foxmail.com>
2025-12-10 20:16:24 +0800

    feat: 删除旧的测试User仓储方法

--- Radish.IService/IUserService.cs
+++ Radish.IService/IUserService.cs
@@ -19,18 +19,9 @@ public interface IUserService : IBaseService<User, UserVo>
     /// </summary>
     /// <returns>List RoleModulePermission</returns>
     Task<List<RoleModulePermission>> RoleModuleMaps();
-
-    #region 初始测试，不用理会
-
-    /// <summary>测试获取用户服务示例</summary>
-    /// <remarks>返回的是视图 Vo 模型，隔离实际实体类</remarks>
-    /// <returns></returns>
-    Task<List<UserVo>> GetUsersAsync();
-
+    
     /// <summary>测试使用同事务</summary>
     /// <remarks>仅为示例，无任何作用</remarks>
     /// <returns></returns>
     Task<bool> TestTranPropagationUser();
-
-    #endregion
 }
