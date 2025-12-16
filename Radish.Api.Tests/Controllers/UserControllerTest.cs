8914d50f30ec87d25bb88b09689a064fcf1b64f3
laugh0608 <laugh0608@foxmail.com>
2025-12-16 20:43:40 +0800

    test: 修复 UserControllerTest 构造函数参数
    - 添加 FakePostService 和 FakeCommentService mock 实现
    - 更新 UserController 构造函数调用，传入所有必需的依赖
    - 实现 IPostService 和 ICommentService 接口的必需方法
    - 所有测试通过

--- Radish.Api.Tests/Controllers/UserControllerTest.cs
+++ Radish.Api.Tests/Controllers/UserControllerTest.cs
@@ -34,8 +34,10 @@ public class UserControllerTest
         var accessor = new HttpContextAccessor { HttpContext = httpContext };
         var httpContextUser = new HttpContextUser(accessor, NullLogger<HttpContextUser>.Instance);
         var userService = new FakeUserService();
+        var postService = new FakePostService();
+        var commentService = new FakeCommentService();
 
-        var controller = new UserController(userService, httpContextUser)
+        var controller = new UserController(userService, httpContextUser, postService, commentService)
         {
             ControllerContext = new ControllerContext
             {
@@ -110,6 +112,89 @@ public class UserControllerTest
             System.Linq.Expressions.Expression<System.Func<T, T2, T3, TResult>> selectExpression,
             System.Linq.Expressions.Expression<System.Func<T, T2, T3, bool>>? whereLambda = null) where T : class, new() => throw new System.NotImplementedException();
         public Task<List<User>> QuerySplitAsync(System.Linq.Expressions.Expression<System.Func<User, bool>>? whereExpression, string orderByFields = "Id") => throw new System.NotImplementedException();
+
+        #endregion
+    }
+
+    private sealed class FakePostService : IPostService
+    {
+        public Task<List<PostVo>> QueryAsync(System.Linq.Expressions.Expression<System.Func<Post, bool>>? whereExpression = null)
+        {
+            return Task.FromResult(new List<PostVo>());
+        }
+
+        public Task<int> QueryCountAsync(System.Linq.Expressions.Expression<System.Func<Post, bool>>? whereExpression = null)
+        {
+            return Task.FromResult(0);
+        }
+
+        #region 未在当前测试中使用的方法
+
+        public Task<long> PublishPostAsync(Post post, List<string>? tagNames = null) => throw new System.NotImplementedException();
+        public Task<PostVo?> GetPostDetailAsync(long postId) => throw new System.NotImplementedException();
+        public Task IncrementViewCountAsync(long postId) => throw new System.NotImplementedException();
+        public Task UpdateLikeCountAsync(long postId, int delta) => throw new System.NotImplementedException();
+        public Task UpdateCommentCountAsync(long postId, int delta) => throw new System.NotImplementedException();
+        public Task<long> AddAsync(Post entity) => throw new System.NotImplementedException();
+        public Task<int> AddRangeAsync(List<Post> entities) => throw new System.NotImplementedException();
+        public Task<List<long>> AddSplitAsync(Post entity) => throw new System.NotImplementedException();
+        public Task<bool> DeleteByIdAsync(long id) => throw new System.NotImplementedException();
+        public Task<bool> DeleteAsync(Post entity) => throw new System.NotImplementedException();
+        public Task<int> DeleteAsync(System.Linq.Expressions.Expression<System.Func<Post, bool>> whereExpression) => throw new System.NotImplementedException();
+        public Task<int> DeleteByIdsAsync(List<long> ids) => throw new System.NotImplementedException();
+        public Task<bool> UpdateAsync(Post entity) => throw new System.NotImplementedException();
+        public Task<int> UpdateRangeAsync(List<Post> entities) => throw new System.NotImplementedException();
+        public Task<bool> UpdateColumnsAsync(Post entity, System.Linq.Expressions.Expression<System.Func<Post, object>> updateColumns) => throw new System.NotImplementedException();
+        public Task<int> UpdateColumnsAsync(System.Linq.Expressions.Expression<System.Func<Post, Post>> updateColumns, System.Linq.Expressions.Expression<System.Func<Post, bool>> whereExpression) => throw new System.NotImplementedException();
+        public Task<PostVo?> QueryByIdAsync(long id) => throw new System.NotImplementedException();
+        public Task<PostVo?> QueryFirstAsync(System.Linq.Expressions.Expression<System.Func<Post, bool>>? whereExpression = null) => throw new System.NotImplementedException();
+        public Task<PostVo?> QuerySingleAsync(System.Linq.Expressions.Expression<System.Func<Post, bool>> whereExpression) => throw new System.NotImplementedException();
+        public Task<(List<PostVo> data, int totalCount)> QueryPageAsync(System.Linq.Expressions.Expression<System.Func<Post, bool>>? whereExpression = null, int pageIndex = 1, int pageSize = 20, System.Linq.Expressions.Expression<System.Func<Post, object>>? orderByExpression = null, SqlSugar.OrderByType orderByType = SqlSugar.OrderByType.Asc) => throw new System.NotImplementedException();
+        public Task<bool> QueryExistsAsync(System.Linq.Expressions.Expression<System.Func<Post, bool>> whereExpression) => throw new System.NotImplementedException();
+        public Task<List<PostVo>> QueryWithCacheAsync(System.Linq.Expressions.Expression<System.Func<Post, bool>>? whereExpression = null, int cacheTime = 10) => throw new System.NotImplementedException();
+        public Task<List<TResult>> QueryMuchAsync<T, T2, T3, TResult>(System.Linq.Expressions.Expression<System.Func<T, T2, T3, object[]>> joinExpression, System.Linq.Expressions.Expression<System.Func<T, T2, T3, TResult>> selectExpression, System.Linq.Expressions.Expression<System.Func<T, T2, T3, bool>>? whereLambda = null) where T : class, new() => throw new System.NotImplementedException();
+        public Task<List<Post>> QuerySplitAsync(System.Linq.Expressions.Expression<System.Func<Post, bool>>? whereExpression, string orderByFields = "Id") => throw new System.NotImplementedException();
+
+        #endregion
+    }
+
+    private sealed class FakeCommentService : ICommentService
+    {
+        public Task<List<CommentVo>> QueryAsync(System.Linq.Expressions.Expression<System.Func<Comment, bool>>? whereExpression = null)
+        {
+            return Task.FromResult(new List<CommentVo>());
+        }
+
+        public Task<int> QueryCountAsync(System.Linq.Expressions.Expression<System.Func<Comment, bool>>? whereExpression = null)
+        {
+            return Task.FromResult(0);
+        }
+
+        #region 未在当前测试中使用的方法
+
+        public Task<long> AddCommentAsync(Comment comment) => throw new System.NotImplementedException();
+        public Task<List<CommentVo>> GetCommentTreeAsync(long postId) => throw new System.NotImplementedException();
+        public Task UpdateLikeCountAsync(long commentId, int delta) => throw new System.NotImplementedException();
+        public Task UpdateReplyCountAsync(long commentId, int delta) => throw new System.NotImplementedException();
+        public Task<long> AddAsync(Comment entity) => throw new System.NotImplementedException();
+        public Task<int> AddRangeAsync(List<Comment> entities) => throw new System.NotImplementedException();
+        public Task<List<long>> AddSplitAsync(Comment entity) => throw new System.NotImplementedException();
+        public Task<bool> DeleteByIdAsync(long id) => throw new System.NotImplementedException();
+        public Task<bool> DeleteAsync(Comment entity) => throw new System.NotImplementedException();
+        public Task<int> DeleteAsync(System.Linq.Expressions.Expression<System.Func<Comment, bool>> whereExpression) => throw new System.NotImplementedException();
+        public Task<int> DeleteByIdsAsync(List<long> ids) => throw new System.NotImplementedException();
+        public Task<bool> UpdateAsync(Comment entity) => throw new System.NotImplementedException();
+        public Task<int> UpdateRangeAsync(List<Comment> entities) => throw new System.NotImplementedException();
+        public Task<bool> UpdateColumnsAsync(Comment entity, System.Linq.Expressions.Expression<System.Func<Comment, object>> updateColumns) => throw new System.NotImplementedException();
+        public Task<int> UpdateColumnsAsync(System.Linq.Expressions.Expression<System.Func<Comment, Comment>> updateColumns, System.Linq.Expressions.Expression<System.Func<Comment, bool>> whereExpression) => throw new System.NotImplementedException();
+        public Task<CommentVo?> QueryByIdAsync(long id) => throw new System.NotImplementedException();
+        public Task<CommentVo?> QueryFirstAsync(System.Linq.Expressions.Expression<System.Func<Comment, bool>>? whereExpression = null) => throw new System.NotImplementedException();
+        public Task<CommentVo?> QuerySingleAsync(System.Linq.Expressions.Expression<System.Func<Comment, bool>> whereExpression) => throw new System.NotImplementedException();
+        public Task<(List<CommentVo> data, int totalCount)> QueryPageAsync(System.Linq.Expressions.Expression<System.Func<Comment, bool>>? whereExpression = null, int pageIndex = 1, int pageSize = 20, System.Linq.Expressions.Expression<System.Func<Comment, object>>? orderByExpression = null, SqlSugar.OrderByType orderByType = SqlSugar.OrderByType.Asc) => throw new System.NotImplementedException();
+        public Task<bool> QueryExistsAsync(System.Linq.Expressions.Expression<System.Func<Comment, bool>> whereExpression) => throw new System.NotImplementedException();
+        public Task<List<CommentVo>> QueryWithCacheAsync(System.Linq.Expressions.Expression<System.Func<Comment, bool>>? whereExpression = null, int cacheTime = 10) => throw new System.NotImplementedException();
+        public Task<List<TResult>> QueryMuchAsync<T, T2, T3, TResult>(System.Linq.Expressions.Expression<System.Func<T, T2, T3, object[]>> joinExpression, System.Linq.Expressions.Expression<System.Func<T, T2, T3, TResult>> selectExpression, System.Linq.Expressions.Expression<System.Func<T, T2, T3, bool>>? whereLambda = null) where T : class, new() => throw new System.NotImplementedException();
+        public Task<List<Comment>> QuerySplitAsync(System.Linq.Expressions.Expression<System.Func<Comment, bool>>? whereExpression, string orderByFields = "Id") => throw new System.NotImplementedException();
 
         #endregion
     }
