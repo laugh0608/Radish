a54091d539e3714600e427d97ebf07fdc0ab503d
laugh0608 <laugh0608@foxmail.com>
2025-12-18 22:55:47 +0800

    test: 修复UserControllerTest接口签名缺少sortBy参数

--- Radish.Api.Tests/Controllers/UserControllerTest.cs
+++ Radish.Api.Tests/Controllers/UserControllerTest.cs
@@ -184,7 +184,7 @@ public class UserControllerTest
         public Task UpdateReplyCountAsync(long commentId, int delta) => throw new System.NotImplementedException();
         public Task<CommentLikeResultDto> ToggleLikeAsync(long userId, long commentId) => throw new System.NotImplementedException();
         public Task<System.Collections.Generic.Dictionary<long, bool>> GetUserLikeStatusAsync(long userId, System.Collections.Generic.List<long> commentIds) => throw new System.NotImplementedException();
-        public Task<List<CommentVo>> GetCommentTreeWithLikeStatusAsync(long postId, long? userId = null) => throw new System.NotImplementedException();
+        public Task<List<CommentVo>> GetCommentTreeWithLikeStatusAsync(long postId, long? userId = null, string sortBy = "newest") => throw new System.NotImplementedException();
         public Task<long> AddAsync(Comment entity) => throw new System.NotImplementedException();
         public Task<int> AddRangeAsync(List<Comment> entities) => throw new System.NotImplementedException();
         public Task<List<long>> AddSplitAsync(Comment entity) => throw new System.NotImplementedException();
